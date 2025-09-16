1fa9c3a43328401b3a1de3bc2ca29ff992610131
farhanMeows <farhanzakiahmad2017@gmail.com>
2025-09-16 15:04:41 +0530

    initial push

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="flex justify-center items-center h-screen">
+        <div className="text-3xl font-bold underline">Hello World</div>
+      </div>
+    </>
+  );
+}
+
+export default App;
